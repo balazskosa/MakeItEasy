7a7578cc27a7b684052152ecf8ea999f3f162b0a
Balázs Kósa <ot4wi3@inf.elte.hu>
2021-10-29 22:31:12 +0200

    refactor User class

--- src/main/java/com/example/makeiteasy/controller/NewProfileScreenController.java
+++ src/main/java/com/example/makeiteasy/controller/NewProfileScreenController.java
@@ -1,6 +1,6 @@
 package com.example.makeiteasy.controller;
 
-import com.example.makeiteasy.User;
+import com.example.makeiteasy.database.pojo.User;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
