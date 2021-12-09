d8e3e2a5c29b74b8e24cdb17c7f2a0a0e0996c21
Balázs Kósa <ot4wi3@inf.elte.hu>
2021-12-09 19:15:27 +0100

    changing day - result page wip

--- src/main/java/com/example/makeiteasy/controller/ResultController.java
+++ src/main/java/com/example/makeiteasy/controller/ResultController.java
@@ -1,4 +1,58 @@
 package com.example.makeiteasy.controller;
 
-public class ResultController {
+import com.example.makeiteasy.database.DB;
+import com.example.makeiteasy.database.pojo.Meal;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+public class ResultController implements Initializable {
+
+    @FXML
+    private BarChart<LocalDate, Number> dailyCalorieChart;
+    @FXML
+    private CategoryAxis calorieIntakeX;
+    @FXML
+    private NumberAxis calorieIntakeY;
+
+    private LocalDate currentDay = LocalDate.now();
+    private ArrayList<Meal> meals = DB.getAllMeals();
+
+    @FXML
+    private Label dayLabel;
+
+    @FXML
+    private void prevDay() {
+        this.currentDay = this.currentDay.minusDays(1);
+        setDayLabel();
+    }
+
+    @FXML
+    private void resetDay() {
+        this.currentDay = LocalDate.now();
+        setDayLabel();
+    }
+
+    @FXML
+    private void nextDay() {
+        this.currentDay = this.currentDay.plusDays(1);
+        setDayLabel();
+    }
+
+    public void setDayLabel() {
+        dayLabel.setText(currentDay.toString());
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        setDayLabel();
+    }
 }
